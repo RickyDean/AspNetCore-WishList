560de677f28fc9e914eee8629aa57742c1ab95e6
Ricky Bottoms <rickydean5@juno.com>
2019-08-08 11:04:48 -0400

    Add DbContext
    Added Folders and classes to support in memory db.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WishList.Data;
 
 namespace WishList
 {
@@ -18,6 +19,7 @@ namespace WishList
 
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
 
